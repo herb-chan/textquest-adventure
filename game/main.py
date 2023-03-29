d6510ee31404ee53bef22936dcb0f07f06c5ee8a
“herbuś” <wujowujo3@gmail.com>
2023-03-29 22:34:15 +0200

    Player statistics
    made Player class which handles all important informations and statistics of your current character. - unfinished as of current state.

--- game/main.py
+++ game/main.py
@@ -1,5 +1,19 @@
 import inquirer
+import random
 
+# creates 'Player' class which handles all important information about the player
+class Player:
+    def __init__(self, name, health, mana, strength, defence, damage, level, ch_class):
+        self.name = name
+        self.health = health
+        self.mana = mana
+        self.strength = strength
+        self.defence = defence
+        self.damage = damage
+        self.level = level
+        self.ch_class = ch_class
+
+# creates an function asking for name on the beginning of each session
 def ask_name():
     while True:
         creation = [
@@ -11,6 +25,16 @@ def ask_name():
         if len(name['name']) == 0:
             continue
         elif len(name['name']) > 0:
-            break
-        else:
+            return name
+        else: # if user is dumb enough to imput something not expected print this >-<
             print('There was an error with validating inputed information.')
+
+# assigns 'name' that has been imputed by the player to variable called name
+name = ask_name()['name']
+# assigns all base stats to player
+player = Player(name, 100, 50, 10, 5, 15, 1, 1)
+
+print(f"Welcome, {player.name}!")
+
+def chest_open():
+    random_number = random.randInt(0,9)
