c83c9aaf9c4c948a79b963f9ad3d394a49ddb1f8
“herbuś” <wujowujo3@gmail.com>
2023-03-30 20:54:41 +0200

    Finished class selection
    Decreased number of available classes from 12 to 4. Every class has their own base stats now.

--- game/main.py
+++ game/main.py
@@ -12,7 +12,7 @@ class Player:
         self.damage = damage
         self.level = level
         self.ch_class = ch_class
-        self.inventory = inventory or []
+        self.inventory = inventory
 
 # creates an function asking for name on the beginning of each session
 def ask_name():
@@ -34,18 +34,17 @@ def ask_name():
 def choose_class():
     options = [
         'Barbarian',
-        'Bard',
-        'Cleric',
-        'Druid',
-        'Fighter',
-        'Monk',
-        'Paladin',
         'Ranger',
-        'Rogue',
-        'Sorcerer',
-        'Warlock',
-        'Wizard'
+        'Wizard',
+        'Necromancer'
     ]
+
+    stats = {
+        'Barbarian': {'health': 120, 'mana': 30, 'strength': 15, 'defence': 10, 'damage': 20},
+        'Ranger': {'health': 100, 'mana': 70, 'strength': 10, 'defence': 8, 'damage': 20},
+        'Wizard': {'health': 80, 'mana': 100, 'strength': 5, 'defence': 10, 'damage': 25},
+        'Necromancer': {'health': 90, 'mana': 90, 'strength': 10, 'defence': 8, 'damage': 15},
+    }
 
     questions = [
     inquirer.List('chosen_class',
@@ -54,14 +53,29 @@ def choose_class():
                   carousel=True
                   ),
     ]
-    answers = inquirer.prompt(questions)
+    chosen_class = inquirer.prompt(questions)['chosen_class']
+
+    if chosen_class in stats:
+        return stats[chosen_class], chosen_class
+    else:
+        print('There was an error with validating chosen class.')
+        return None, None
 
 # assigns 'name' that has been imputed by the player to variable called name
 name = ask_name()['name']
-# assigns all base stats to player
-player = Player(name, 100, 50, 10, 5, 15, 1, 1)
+chosen_stats, chosen_class = choose_class()
 
-print(f"Welcome, {player.name}!")
+if chosen_stats is not None:
+    # assigns all base stats to player based on the chosen class
+    player = Player(name, chosen_stats['health'], chosen_stats['mana'], chosen_stats['strength'], chosen_stats['defence'], chosen_stats['damage'], 1, chosen_class, [])
+    print(f"Name: {player.name}")
+    print(f"Health: {player.health}")
+    print(f"Mana: {player.mana}")
+    print(f"Strength: {player.strength}")
+    print(f"Defence: {player.defence}")
+    print(f"Damage: {player.damage}")
+    print(f"Level: {player.level}")
+    print(f"Class: {player.ch_class}")
+    print(f"Inventory: {player.inventory}")
 
-def chest_open():
-    random_number = random.randInt(0,9)
+print(f"Welcome, {player.name}! You'll be playing as {chosen_class}")
